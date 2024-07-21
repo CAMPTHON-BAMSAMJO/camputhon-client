fa9f821b81346eddcfa2899598f5b6cbcd71f330
emjayMJkim <minjeong030420@gmail.com>
2024-07-21 20:18:47 +0900

    [feat] bottom navigation

--- app/src/main/java/com/dgu/camputhon/presentation/MainActivity.kt
+++ app/src/main/java/com/dgu/camputhon/presentation/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.dgu.camputhon.presentation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.dgu.camputhon.R
 import com.dgu.camputhon.databinding.ActivityMainBinding
-import com.dgu.camputhon.presentation.sample.SampleFragment
+import com.dgu.camputhon.presentation.home.HomeFragment
+import com.dgu.camputhon.presentation.plus.PlusFragment
+import com.dgu.camputhon.presentation.store.StoreFragment
 import com.dgu.camputhon.util.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,13 +17,38 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         initFragment()
+        initBottomNavigation()
+        setBottomNavigationClickListener()
+    }
+
+    private fun setBottomNavigationClickListener() {
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_navigation_plus -> changeFragment(PlusFragment())
+                R.id.bottom_navigation_home -> changeFragment(HomeFragment())
+                R.id.bottom_navigation_store -> changeFragment(StoreFragment())
+                else -> true
+            }
+        }
     }
 
     private fun initFragment() {
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main_test)
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
         if (currentFragment == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.fcv_main_test, SampleFragment()).commit()
+                .add(R.id.fcv_main, HomeFragment()).commit()
         }
+    }
+
+    private fun changeFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commit()
+        return true
+    }
+
+    private fun initBottomNavigation() {
+        binding.bnvMain.selectedItemId = R.id.bottom_navigation_home
+        binding.bnvMain.itemIconTintList = null
     }
 }
