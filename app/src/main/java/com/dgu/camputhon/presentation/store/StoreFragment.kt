97307abb2ac78f6430e935178579feaa872a0132
emjayMJkim <minjeong030420@gmail.com>
2024-07-22 08:25:52 +0900

    [feat] 숏폼 저장 서버통신 구현

--- app/src/main/java/com/dgu/camputhon/presentation/store/StoreFragment.kt
+++ app/src/main/java/com/dgu/camputhon/presentation/store/StoreFragment.kt
@@ -2,6 +2,7 @@ package com.dgu.camputhon.presentation.store
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.dgu.camputhon.R
 import com.dgu.camputhon.databinding.FragmentStoreBinding
 import com.dgu.camputhon.util.base.BaseFragment
@@ -10,7 +11,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class StoreFragment : BaseFragment<FragmentStoreBinding>(R.layout.fragment_store) {
 
+    private val viewModel by viewModels<StoreViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getShortsItem()
     }
 }
